2b459f93281dd107ab85ce46e43ad0565b6675b3
SSLL-2025 <rodrigo.caminada@psinet.cl>
2025-08-04 17:37:57 -0400

    Update email.controller.js

--- controller/email.controller.js
+++ controller/email.controller.js
@@ -49,12 +49,12 @@ const sendEmailCompra = async (req, res) => {
     let correo = "rcami001@contratistas.codelco.cl";
     let nombre = "";
     let correosCopia = ['rcami001@contratistas.codelco.cl',
-      'mbust001@contratistas.codelco.cl',
-      'apena020@contratistas.codelco.cl',
-      "ggodo009@contratistas.codelco.cl",
-      "zgome001@codelco.cl",
-      "lmont013@contratistas.codelco.cl",
-      "marzule@contratistas.codelco.cl"];
+     // 'mbust001@contratistas.codelco.cl',
+     // 'apena020@contratistas.codelco.cl',
+     // "ggodo009@contratistas.codelco.cl",
+     // "zgome001@codelco.cl",
+      "lmont013@contratistas.codelco.cl""];
+    //  "marzule@contratistas.codelco.cl"];
     let nombre_compra = request.cardname;
     let link_tarjeta = request.link_card;
     let link_tablero = request.link_board;
