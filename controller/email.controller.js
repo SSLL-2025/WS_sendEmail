51a1e44b2a6c96ba8fbb95704d0cccb3464d1a5f
SSLL-2025 <rodrigo.caminada@psinet.cl>
2025-10-07 10:07:34 -0300

    Update email.controller.js

--- controller/email.controller.js
+++ controller/email.controller.js
@@ -48,13 +48,13 @@ const sendEmailCompra = async (req, res) => {
     //Sacar el correo por el tipo de etiqueta.
     let correo = "rcami001@contratistas.codelco.cl";
     let nombre = "";
-    let correosCopia = ['r.caminada.aca@gmail.com',
-     'mbust001@contratistas.codelco.cl',
-     'apena020@contratistas.codelco.cl',
-     'ggodo009@contratistas.codelco.cl',
-     'zgome001@codelco.cl',
-      'lmont013@contratistas.codelco.cl',
-       'marzule@contratistas.codelco.cl'];
+    let correosCopia = ["r.caminada.aca@gmail.com",
+     "mbust001@contratistas.codelco.cl",
+     "apena020@contratistas.codelco.cl",
+     "ggodo009@contratistas.codelco.cl",
+     "zgome001@codelco.cl",
+      "lmont013@contratistas.codelco.cl",
+       "marzule@contratistas.codelco.cl"];
     let nombre_compra = request.cardname;
     let link_tarjeta = request.link_card;
     let link_tablero = request.link_board;
@@ -89,6 +89,8 @@ const sendEmailCompra = async (req, res) => {
       correo = "jbrit008@contratistas.codelco.cl"
      nombre = "Javier Brito"
    }  else {
+        correo = "rcami001@contratistas.codelco.cl"
+     nombre = "Rodrigo Caminada"
     }
   
     //SACAR LOS ADJUNTOS DE LA TARJETA TRELLO MEDIANTE LA API
